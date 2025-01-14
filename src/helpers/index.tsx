79d2f77607eadf026c4ed47afd7d27f6a074df66
Gabriel Bustos <gabrielbustosdev@gmail.com>
2025-01-14 00:49:54 -0300

    remove expense

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,3 +1,14 @@
 export function formatCurrency(amaount: number){
     return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(amaount)
 }
+
+export function formatDate(dateStr: string) : string {
+    const dateObj = new Date(dateStr)
+    const options : Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return new Intl.DateTimeFormat('es-ES', options).format(dateObj)
+}
