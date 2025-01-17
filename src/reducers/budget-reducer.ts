4d3dbf6fcc6a1873c37cc58cfe9a30e309d87a5d
Gabriel Bustos <gabrielbustosdev@gmail.com>
2025-01-17 02:58:08 -0300

    Refactor budget management: Implement localStorage sync, enhance expense handling, and improve state management. Added editing functionality in ExpenseForm and updated BudgetTracker to reflect dynamic budget values.

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -6,19 +6,33 @@ export type BudgetActions =
     { type: 'show-modal' } |
     { type: 'close-modal' } |
     { type: 'add-expense', payload: {expense: DraftExpense } } | 
-    { type: 'remove-expense', payload: {id: Expense['id']} }
+    { type: 'remove-expense', payload: {id: Expense['id']} } | 
+    { type: 'get-expense-by-id', payload: {id: Expense['id']} } |
+    { type: 'update-expense', payload: {expense: Expense} }
 
 
 export type BudgetState = { 
     budget: number
     modal: boolean
     expenses: Expense[]
+    editingId: Expense['id']
+}
+
+const initialBudget = () : number => {
+    const localStorageBudget = localStorage.getItem('budget')
+    return localStorageBudget ? +localStorageBudget : 0
+}
+
+const initialExpenses = () : Expense[] => {
+    const localStorageExpenses = localStorage.getItem('expenses')
+    return localStorageExpenses ? JSON.parse(localStorageExpenses) : []
 }
 
 export const initialState : BudgetState = { 
-    budget: 0,
+    budget: initialBudget(),
     modal: false,
-    expenses: []
+    expenses: initialExpenses(),
+    editingId: ''
 }
 
 const createExpense = (draftExpense: DraftExpense) : Expense => {
@@ -50,7 +64,8 @@ export const budgetReducer = (
     if(action.type === 'close-modal') {
         return {
             ...state,
-            modal: false
+            modal: false,
+            editingId: ''
         }
     }
 
@@ -69,6 +84,23 @@ export const budgetReducer = (
         return {
             ...state,
             expenses: state.expenses.filter(expense => expense.id !== action.payload.id)
+        }
+    }
+
+    if(action.type === 'get-expense-by-id') {
+        return {
+            ...state,
+            editingId: action.payload.id,
+            modal: true
+        }
+    }
+
+    if(action.type === 'update-expense') {
+        return {
+            ...state,
+            expenses: state.expenses.map(expense => expense.id === action.payload.expense.id ? action.payload.expense : expense),
+            modal: false,
+            editingId: ''
         }
     }
 
