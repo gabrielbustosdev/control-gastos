af9965d92989c83f4567674dcf8ccb99e0b3a724
Gabriel Bustos <gabrielbustosdev@gmail.com>
2025-01-20 12:33:55 -0300

    Refactor ExpenseList component: Update isEmpty calculation to depend on filteredExpenses instead of state.expenses for improved performance and accuracy in rendering empty state.

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -8,7 +8,7 @@ export default function ExpenseList() {
 
     const filteredExpenses = state.currentCategory ? state.expenses.filter(expense => expense.category === state.currentCategory) : state.expenses
     
-    const isEmpty = useMemo(() => filteredExpenses.length === 0, [state.expenses])
+    const isEmpty = useMemo(() => filteredExpenses.length === 0, [filteredExpenses])
 
     return (
         <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
